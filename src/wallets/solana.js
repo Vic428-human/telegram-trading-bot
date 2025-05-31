32e032c1347e183d1776a1215afa96aa24e9155e
Vic428-human <z0983195379a@gmail.com>
2025-05-31 09:39:23 +0800

    配置Methods for erc20 abi

---
+++ src/wallets/solana.js
@@ -0,0 +1,17 @@
+const { ethers } = require("ethers");
+const axios = require("axios");
+require("dotenv").config();
+
+// 1 inch router 可以做 swap 
+// https://ethereum.org/zh-tw/developers/docs/standards/tokens/erc-20/
+const abi = [
+    // Read-Only Functions
+    "function balanceOf(address owner) view returns (uint256)", // 查詢指定地址的代幣餘額
+    "function decimals() view returns (uint8)", // 取得代幣的小數位數
+    "function symbol() view returns (string)", // 取得代幣的符號（如 ETH、USDT）
+    // 你允許某個人（或機器人、合約）幫你花你錢包裡的一部分代幣，但有一個上限。
+    "function approve(address _spender, uint256 _value) public returns (bool success)", // Solidity 0.5.0 之後，所有函數必須明確標註可見性（如 public、external、internal、private）。早期版本若未標註則預設為 public。現代合約建議明確寫出 public 或 external
+    // 查詢你已經允許某個人（或合約）最多還能幫你花多少代幣。
+    "function allowance(address _owner, address _spender) public view returns (uint256 remaining)" // remaining 只是返回值的命名，對外部呼叫者沒差別。ERC-20 標準原型是 returns (uint256)，但加上名稱可提升可讀性。
+];
+
