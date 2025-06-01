9dd42a1dda527d75f7aca61eb300ac1e34565d06
Vic428-human <z0983195379a@gmail.com>
2025-06-01 09:01:06 +0800

    feat: getEvmBalance 1.從morails獲取 Native & ERC20 Token Balances基於地址 2.沒有資料時的錯誤判斷 3.有資料時，另外map兩種數據資料 a.native token 跟 erc20 token 包含rename 4.try catch 錯誤產生時，錯誤訊息的提示

--- src/wallets/evm.js
+++ src/wallets/evm.js
@@ -30,8 +30,79 @@ const getEvmProvider = (chain) => {
 // 獲取EVM wallet address => https://docs.ethers.org/v5/api/signer/#Wallet
 // Create a new Wallet instance for privateKey and optionally connected to the provider.
 const getEvmSigner = (chain) => {
+    const provider = getEvmProvider(chain);
     const privateKey = process.env.PRIVATE_KEY; // privateKey is the 32-byte hexadecimal private key of the wallet,
     // const provider = new ethers.providers.InfuraProvider("mainnet"); 
-    const wallet = new ethers.Wallet(privateKey, getEvmProvider(chain));
+    const wallet = new ethers.Wallet(privateKey, provider);
     return wallet; // console.log("Wallet address:", wallet.address);
-}   
+};
+
+// use axios .get 
+const getEvmBalance = async (chain) => {
+    try{
+    const wallet = getEvmSigner(chain)
+        const walletAddress = wallet.address; 
+        // Get Native & ERC20 Token Balances by Wallet 基於 cURL 的方式 => https://docs.moralis.com/web3-data-api/evm/reference/get-wallet-token-balances-price?address=0xcB1C1FdE09f811B294172696404e88E658659905&chain=eth&token_addresses=[null]
+        // https://deep-index.moralis.io/api/v2.2/wallets/<將用來查詢代幣餘額的地址>/tokens?chain=eth
+        const response = await axios.get(`https://deep-index.moralis.io/api/v2.2/wallets/${walletAddress}/tokens`,  {
+            params: {
+                chain: chain.moralisChainName
+            },
+            headers: {
+                accept: "application/json",
+                "X-API-Key": process.env.MORALIS_API_KEY
+            }
+        });
+
+        if(!response.data || !response.data.result) {
+        throw new Error(`Native & ERC20 Token Balances by Wallet error,請確認錢包地址： ${wallet.address}, 以及鏈名：${chain.moralisChainName} 是否正確，倘若不正確，表示 /add <address> <chainID>指令的時候就新增錯誤了`);
+        } 
+
+        const result = response.data.result; 
+        // 找出 native_token 為 true 的物件
+        const nativeToken = result.find(token => token.native_token ===  true);
+
+        // 找出其餘ERC20的token 
+        const ERC20Tokens = result.filter(token => token.native_token === false).map((ERC20Token)=>{
+            return {
+                name: ERC20Token.name,
+                symbol: ERC20Token.symbol,
+                decimals: ERC20Token.decimals,
+                address: ERC20Token.token_address,
+                balance: ERC20Token.balance,
+                usdValue: ERC20Token.usd_value
+            }
+        });
+
+        return {
+            address: walletAddress,
+            nativeToken: nativeToken ? 
+            {
+                symbol: nativeToken.symbol,
+                amount: nativeToken.amount,
+                usdValue: nativeToken.usd_value
+            } : {
+                symbol: chain.nativeToken.symbol,   
+                amount: "0",
+                usdValue: 0,
+            },
+            ERC20Tokens
+        }
+    } catch (error) {
+        const API_RESOURCE = 'https://docs.moralis.com/web3-data-api/evm/reference/get-wallet-token-balances-price?address=0xcB1C1FdE09f811B294172696404e88E658659905&chain=eth&token_addresses=[]';
+        console.error(`在 ${chain.moralisChainName} 鏈，chainId： ${chain.chainId}，獲取錢包餘額時發生錯誤 `,error.message);
+        return { 
+            API_RESOURCE: API_RESOURCE,
+            nativeToken: {
+                symbol: chain.nativeToken.symbol,   
+                amount: "0",
+                usdValue: 0,
+            },
+            ERC20Tokens: []
+        };
+    }
+   
+
+
+}
+
